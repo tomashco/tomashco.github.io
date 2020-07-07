3a55f5fcea1312934147ec2cc4bf4b299d061da8
Tommaso Maioli <tommaso.maioli@gmail.com>
2020-07-07 22:21:14 +0200

    Updates

---
+++ precache-manifest.f896a549821e347c2551ed9e728bbb50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f616cc1d72bd232113c3c9f8f822459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b16a8a164c374743a9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ab2a3c8125f885d8fb4",
+    "url": "/static/js/2.009b76cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.009b76cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b16a8a164c374743a9",
+    "url": "/static/js/main.0fe70435.chunk.js"
+  },
+  {
+    "revision": "605c78b83f012eab7045",
+    "url": "/static/js/runtime-main.9654fdce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
