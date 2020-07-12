609a3ba03c9365987e371fae3fd4514629a4474a
Tommaso Maioli <tommaso.maioli@gmail.com>
2020-07-13 00:25:48 +0200

    added collapsible social menu on small screen. Still needs to tweak the style

--- src/AppBar.js
+++ src/AppBar.js
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import MailIcon from '@material-ui/icons/Mail';
+
+import ButtonAppBarCollapse from './ButtonAppBarCollapse'
 // import {Anchor, Box, Heading, Nav} from 'grommet'
 // import {Home, Notification, ChatOption} from 'grommet-icons'
 
@@ -14,7 +16,24 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "space-between",
     alignItems: "center",
     //marginBottom: "2em"
-  }
+  },
+  flexButton: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent:"flex-end",
+  },
+  buttonBar: {
+    [theme.breakpoints.down("xs")]: {
+      display: "none",
+    },
+    // margin: "10px",
+    // paddingLeft: "16px",
+    // right: 0,
+    // position: "relative",
+    // width: "100%",
+    //background: "transparent",
+
+  },
 }));
 
 const AppBar = (props) => {
@@ -25,10 +44,19 @@ const AppBar = (props) => {
       <Typography variant="h1" style={{fontFamily: "Modak", color: "rgba(0, 0, 0, 0.54)", fontSize:'3rem'}}>
       Tomashco
       </Typography>
-      <div style={{display: "flex", alignItems: "center"}}>
-        <GitHubIcon style={{fontSize:"2em"}}/>
-        <LinkedInIcon style={{fontSize:"2.5em"}}/>
-        <MailIcon style={{fontSize:"2.5em"}}/>
+        <ButtonAppBarCollapse>
+      <div className={classes.flexButton} style={{backgroundColor: "#ff4545"}}>
+          <GitHubIcon style={{fontSize:"2em"}}/>
+          <LinkedInIcon style={{fontSize:"2.5em"}}/>
+          <MailIcon style={{fontSize:"2.5em"}}/>
+      </div>
+        </ButtonAppBarCollapse>
+        <div className={classes.buttonBar} id="appbar-collapse">
+        <div className={classes.flexButton}>
+          <GitHubIcon style={{fontSize:"2em"}}/>
+          <LinkedInIcon style={{fontSize:"2.5em"}}/>
+          <MailIcon style={{fontSize:"2.5em"}}/>
+        </div>
       </div>
     </div>
     )
